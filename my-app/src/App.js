2cdbb5070b71698e166a5c48b43bfef9e3a23104
DmitriiVKh <hdv.kub@gmail.com>
2023-10-16 00:00:00 +0300

    without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-export const App = () => {
-  const year = new Date().getFullYear();//Императивный стиль
+export function App() {
+  const year = new Date().getFullYear();
 
-  //Ниже декларативный стиль
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        <div>
-        {year} 
-        </div>
-
-      </header>
-    </div>
+  const header = createElement(
+    'header',
+    { className: 'App-header' },
+    createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+    createElement('p', null, 'Edit ', createElement('code', null, 'src/App.js'), ' and save to reload.'),
+    createElement(
+      'a',
+      {
+        className: 'App-link',
+        href: 'https://reactjs.org',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      },
+      'Learn React'
+    ),
+    createElement('div', null, year)
   );
-};
 
+  return createElement(
+    'div',
+    { className: 'App' },
+    header
+  );
+}
 
+export default App;
